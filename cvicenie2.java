3bdde5bf3025a97a989dc9437a964b7860784685
Marco <xnemethm5@stuba.sk>
2023-09-27 21:29:22 +0200

    Cvicenie2

--- cvicenie2.java
+++ cvicenie2.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class cvicenie2 {
     public static void main(String[] args) {
-            System.out.println("Hello, World!");
+            System.out.println("Hello, World!!!");
         }
 }
